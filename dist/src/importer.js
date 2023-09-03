92103e2c53d14e2c3104521781bf64aaa30abd80
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 17:56:29 +0330

    feat/faeze/js-exam/src/importer.ts

---
+++ dist/src/importer.js
@@ -0,0 +1,5 @@
+export const inputNumber = document.querySelector("#inputNumber");
+export const firstButton = document.querySelector("#firstButton");
+export const secondButton = document.querySelector("#secondButton");
+export const thirdButton = document.querySelector("#thirdButton");
+//# sourceMappingURL=importer.js.map
---
+++ src/importer.ts
@@ -0,0 +1,4 @@
+export const inputNumber = document.querySelector<HTMLInputElement>("#inputNumber");
+export const firstButton = document.querySelector<HTMLButtonElement>("#firstButton");
+export const secondButton = document.querySelector<HTMLButtonElement>("#secondButton");
+export const thirdButton = document.querySelector<HTMLButtonElement>("#thirdButton");
