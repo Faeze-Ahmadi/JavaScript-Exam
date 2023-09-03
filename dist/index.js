6777073b619c89c8a4b07b4dbdcdfce5b696c929
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 19:11:21 +0330

    feat/faeze/js-exam/displayNumberList

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,25 @@ const inputNumber = document.querySelector("#inputNumber");
 const firstButton = document.querySelector("#firstButton");
 const secondButton = document.querySelector("#secondButton");
 const thirdButton = document.querySelector("#thirdButton");
+const numberList = [];
 const generateRandomNumber = () => {
     if (firstButton && inputNumber) {
         firstButton.addEventListener("click", () => {
             const randomNumber = Math.floor(Math.random() * 90000) + 10000;
             inputNumber.value = randomNumber.toString();
             const numberValue = parseInt(inputNumber.value);
+            numberList.push(numberValue);
             console.log(numberValue);
         });
     }
 };
+const displayNumberList = () => {
+    if (secondButton) {
+        secondButton.addEventListener("click", () => {
+            console.log(numberList);
+        });
+    }
+};
 generateRandomNumber();
+displayNumberList();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ const firstButton = document.querySelector<HTMLButtonElement>("#firstButton");
 const secondButton = document.querySelector<HTMLButtonElement>("#secondButton");
 const thirdButton = document.querySelector<HTMLButtonElement>("#thirdButton");
 
+const numberList: number[] = [];
 
 const generateRandomNumber = () => {
     if (firstButton && inputNumber) {
@@ -10,9 +11,19 @@ const generateRandomNumber = () => {
             const randomNumber = Math.floor(Math.random() * 90000) + 10000;
             inputNumber.value = randomNumber.toString();
             const numberValue = parseInt(inputNumber.value);
+            numberList.push(numberValue);
             console.log(numberValue);
         });
     }
 };
 
+const displayNumberList = () => {
+    if (secondButton) {
+        secondButton.addEventListener("click", () => {
+            console.log(numberList);
+        });
+    }
+};
+
 generateRandomNumber();
+displayNumberList();
